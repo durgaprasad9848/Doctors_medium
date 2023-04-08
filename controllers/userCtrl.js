46a40b21116f1b8e692d9e038a7311d1d1fdbe79
durgaprasad9848 <kanakadurgaprasad.p@gmail.com>
2023-04-08 13:54:04 +0300

    Protected routes and layout complete

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -47,6 +47,7 @@ const loginController = async (req, res) => {
 const authController = async (req, res) => {
   try {
     const user = await userModel.findOne({ _id: req.body.userId });
+    user.password = undefined;
     if (!user) {
       return res.status(200).send({
         message: 'user not found',
@@ -55,10 +56,7 @@ const authController = async (req, res) => {
     } else {
       res.status(200).send({
         success: true,
-        data: {
-          name: user.name,
-          email: user.email,
-        },
+        data: user,
       });
     }
   } catch (error) {
